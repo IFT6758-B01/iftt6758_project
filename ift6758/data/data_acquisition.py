7b660a9411fc0faba995116342c18b077f33c4b5
Yuan Li <liyuan.montreal@gmail.com>
2024-10-15 15:22:45 -0400

    add feature for playoff data

--- ift6758/data/data_acquisition.py
+++ ift6758/data/data_acquisition.py
@@ -62,13 +62,11 @@ class NHLDataFetcher:
 
     def get_game_data_by_id(self, game_id):
         season = game_id[5:9]
-        file_path = self.save_dir+'/'+season+'/'+game_id+'.json'
-        #print(file_path)
+        file_path = self.save_dir+season+'/'+game_id+'.json'       
         try:
             if os.path.exists(file_path):
                 with open(file_path, 'r') as f:
-                    game_data = json.load(f)
-                    #print('game_data_id',game_data)
+                    game_data = json.load(f)                    
             else:
                 response = requests.get(self.BASE_URL.format(game_id))
                 if response.status_code == 200:
