dab71f097bd7f0bff583121341db99d45dac2e6b
linkai-aries-ma <arieslkm@gmail.com>
2024-11-22 20:29:22 -0500

    added feature selection graphs to the complex xgboost as well as adding a plotting function, see neural network or complex xgboost for usage details

--- ift6758/advanced_models/xgboost/xgboost_complex-wandb.py
+++ ift6758/advanced_models/xgboost/xgboost_complex-wandb.py
@@ -2,9 +2,9 @@
 
 #TODO:
 # -Figure out how to deal with NaN values in X (see lines 79-80)
-# -LabelEncode non-numerical features in X ?
-# -Log relevant figures to WandB
 
+import sys
+sys.path.append('../')
 import wandb
 import pandas as pd
 import numpy as np
@@ -14,94 +14,11 @@ import os
 from sklearn import preprocessing
 #from sklearn.grid_search import GridSearchCV
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, roc_curve, auc
-from sklearn.calibration import CalibrationDisplay
+from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
-
-# taking this out cuz it will also run the xgboost in the other file
-#from xgboost_simple_wandb import roc_curve_and_auc, goal_rate_by_percentile, cumulative_proportion_of_goals, reliability_diagram
 import matplotlib.pyplot as plt
+from plotting import roc_curve_and_auc, goal_rate_by_percentile, cumulative_proportion_of_goals, reliability_diagram
 
-# Instead including the plotting function here
-## 1. ROC Curve and AUC
-def roc_curve_and_auc(y_val, y_prob, log_to_run=None):
-    fpr, tpr, _ = roc_curve(y_val, y_prob)
-    roc_auc = auc(fpr, tpr)
-
-    # Plot ROC Curve
-    fig = plt.figure(figsize=(10, 6))
-    plt.plot(fpr, tpr, label=f"XGBoost Classifier (AUC = {roc_auc:.2f})")
-    plt.plot([0, 1], [0, 1], 'k--', label="Random Classifier (AUC = 0.50)")
-    plt.title("Receiver Operating Characteristic (ROC) Curve")
-    plt.xlabel("False Positive Rate")
-    plt.ylabel("True Positive Rate")
-    plt.legend(loc="lower right")
-    if log_to_run:
-        if wandb.run is not None:
-            run.log({'roc_auc': plt})
-    plt.show()
-    plt.close(fig)
-       
-## 2. Goal Rate by Percentile (Binned by 5%)
-def goal_rate_by_percentile(y_val, y_prob, log_to_run=None):
-    df_val = pd.DataFrame({'y_val': y_val, 'y_prob': y_prob})
-    df_val['percentile'] = pd.qcut(df_val['y_prob'], 100, labels=False, duplicates='drop') + 1  # Percentiles from 1 to 100
-    goal_rate_by_percentile = df_val.groupby('percentile')['y_val'].mean()
-
-    fig, ax = plt.subplots(figsize=(10, 6))
-    ax.plot(goal_rate_by_percentile.index, goal_rate_by_percentile, marker='o')
-    ax.set_title("Goal Rate by Percentile")
-    ax.set_xlabel("Shot Probability Model Percentile")
-    ax.set_ylabel("Goal Rate (#goals / (#goals + #no_goals))")
-    ax.set_ylim(0, 1)  # Set the y-axis range from 0 to 1
-    ax.grid(True)
-    ax.invert_xaxis()  # Reverse the x-axis
-
-    if log_to_run:
-        log_to_run.log({'Goal Rate by Percentile': wandb.Image(fig)})
-    plt.show()
-    plt.close(fig)
-    
-## 3. Cumulative Proportion of Goals by Percentile
-def cumulative_proportion_of_goals(y_val, y_prob, log_to_run=None):
-    df_val = pd.DataFrame({'y_val': y_val, 'y_prob': y_prob})
-    df_val = df_val.sort_values('y_prob', ascending=False).reset_index(drop=True)  # Sort by descending probability
-
-    # Calculate cumulative goals and proportion
-    cumulative_goals = df_val['y_val'].cumsum()
-    total_goals = df_val['y_val'].sum()
-    cumulative_goal_percentage = cumulative_goals / total_goals
-
-    # Percentile bins (from 100% to 0%)
-    percentiles = np.linspace(100, 0, len(cumulative_goal_percentage))
-
-    # Plot cumulative proportion of goals
-    fig, ax = plt.subplots(figsize=(10, 6))
-    ax.plot(percentiles, cumulative_goal_percentage, marker='o', label="Cumulative Proportion")
-    ax.set_title("Cumulative Proportion of Goals by Model Percentile")
-    ax.set_xlabel("Shot Probability Model Percentile")
-    ax.set_ylabel("Cumulative Proportion of Goals")
-    ax.grid(True)
-    ax.invert_xaxis()  # Reverse the x-axis
-
-    if log_to_run:
-        log_to_run.log({'Cumulative Proportion of Goals': wandb.Image(fig)})
-    plt.show()
-    plt.close(fig)
-
-# 4. Reliability Diagram (Calibration Curve)
-def reliability_diagram(y_val, y_prob, log_to_run=None):
-    CalibrationDisplay.from_predictions(y_val, y_prob, n_bins=30, strategy='uniform')
-    plt.title("Reliability Diagram (Calibration Curve)")
-    if log_to_run:
-        log_to_run.log({f"Reliability Diagram": wandb.Image(plt)})
-    '''
-    if log_to_run:
-        if wandb.run is not None:
-            run.log({'reliability_diagram': plt})
-            return
-    '''
-    plt.show()
 
 # Set paths
 try:
@@ -131,23 +48,22 @@ run = wandb.init(entity="IFT6758_2024-B01" ,project="ms2-xgboost-models", name="
 
 # Feature selection (basic) and NaN values processing
 features = [
-    'distance_from_net',
-    'angle_from_net',
-    'game_seconds',
-    'period',
-    'x_coord',
-    'y_coord',
-    'distance_from_net',
-    'angle_from_net',
-    'shot_type',
-    'last_event_type',
-    'last_x_coord',
-    'last_y_coord',
-    'time_from_last_event',
-    'distance_from_last_event',
-    'rebound',
-    'change_in_shot_angle',
-    'speed'
+    'distance_from_net', # f0
+    'angle_from_net', # f1  #
+    'game_seconds', # f2  #
+    'period', # f3  ###
+    'x_coord', # f4  ###
+    'y_coord', # f5
+    'angle_from_net', # f6  #
+    'shot_type', # comment this out when selecting features
+    'last_event_type', # comment this out when selecting features
+    'last_x_coord', # f7  ###
+    'last_y_coord', # f8  ##
+    'time_from_last_event', # f9
+    'distance_from_last_event', # f10  ##
+    'rebound', # f11
+    'change_in_shot_angle', # f12  ###
+    'speed' # f13  ##
 ]
 
 df['distance_from_last_event'] = df['distance_from_last_event'].fillna(df['distance_from_last_event'].mean())
@@ -166,8 +82,6 @@ if not df_percent_nan.empty:
     print('[WARNING] The following features have lots of NaN values')
     print(df_percent_nan)
     print('Current method of inputation is replacing with mean')
-## =====> THIS IS CURRENTLY FAILING <=====
-# X.fillna(X.mean(), inplace=True)
 
 # Let XGBoost handle NaN values
 if X.isnull().sum().sum() > 0:
@@ -176,7 +90,7 @@ if X.isnull().sum().sum() > 0:
 y = df['is_goal']
 y.fillna(0, inplace=True)
 
-X = pd.get_dummies(X, columns=['shot_type', 'last_event_type'], drop_first=True)
+X = pd.get_dummies(X, columns=['shot_type', 'last_event_type'], drop_first=True) # comment this out when selecting features
 
 # Scale numerical features
 scaler = StandardScaler()
@@ -187,19 +101,6 @@ X = scaler.fit_transform(X)
 
 # Split into training and validation sets
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
-
-'''
-# LabelEncode non-numeric features
-# From https://www.kaggle.com/code/phunter/xgboost-with-gridsearchcv
-# Iterate through columns of X_train
-for f in X.columns:
-    if X[f].dtype=='object':
-        print(f)
-        lbl = preprocessing.LabelEncoder()
-        lbl.fit(list(X_train[f].values) + list(X_val[f].values))
-        X_train[f] = lbl.transform(list(X_train[f].values))
-        X_val[f] = lbl.transform(list(X_val[f].values))
-'''
 
 # Transform data to DMatrix data structure so that we can use xgb.cv
 # Reference: https://datascience.stackexchange.com/questions/12799/pandas-dataframe-to-dmatrix
@@ -251,90 +152,20 @@ goal_rate_by_percentile(y_val, y_val_pred_prob, log_to_run=run)
 cumulative_proportion_of_goals(y_val, y_val_pred_prob, log_to_run=run)
 reliability_diagram(y_val, y_val_pred_prob, log_to_run=run)
 
-'''
-# Instanciate XGBoost Classifier
-xg_clf = xgb.XGBClassifier()
+xgb.plot_importance(xgb_clf, importance_type='gain')  # Change to 'weight' or 'cover' if needed
+plt.title("Feature Importance by Gain")
+plt.show()
 
-# Set hyperparameters
-params = {
-    'objective':'binary:logistic',
-    'max_depth': 4,
-    'alpha': 10,
-    'learning_rate': 1.0,
-    'n_estimators':100
-}
+xgb.plot_importance(xgb_clf, importance_type='weight')  
+plt.title("Feature Importance by Weight")
+plt.show()
 
-# Operate GridSearch CrossValidation
-clf = GridSearchCV(xg_clf,
-                   params,
-                   n_jobs=5,
-                   cv=StratifiedKFold(train['QuoteConversion_Flag'], n_folds=5, shuffle=True),
-                   scoring='accuracy')
-
-clf.fit(X_train, y_train)
-
-
-#From https://www.kaggle.com/code/phunter/xgboost-with-gridsearchcv
-best_parameters, score, _ = max(clf.grid_scores_, key=lambda x: x[1])
-print('Accuracy score:', score)
-for param_name in sorted(best_parameters.keys()):
-    print("%s: %r" % (param_name, best_parameters[param_name]))
-
-test_probs = clf.predict_proba(X_test)[:,1]
-
-# Saving model to JSON
-model_basename = 'xgb_classifier_complex.json'
-model_path = current_dirpath / model_basename
-clf.save_model(fname=model_path)
-'''
+xgb.plot_importance(xgb_clf, importance_type='cover')  
+plt.title("Feature Importance by Cover")
+plt.show()
 
 # Log model to WandB run
 run.log_model(path=model_path, name=model_basename)
 
 # End run
 run.finish()
-
-'''
-# XGBoost model instance
-xg_clf = xgb.XGBClassifier(**params)
-xg_clf.fit(X_train, y_train)
-
-# XGBoost model predictions
-y_pred = xg_clf.predict(X_val)
-y_probas = xg_clf.predict_proba(X_val)
-
-# Print feature importance
-xgb.plot_importance(xg_clf)
-plt.figure(figsize=(10, 6))
-run.log({'feature_importance': plt})
-
-# Print accuracy
-accuracy = accuracy_score(y_val, y_pred)
-print(f"XGBoost Classifier Accuracy score: {accuracy}")
-run.log({'accuracy': accuracy)
-
-# Saving model to JSON
-model_basename = 'xgb_classifier_simple.json'
-model_path = current_dirpath / model_basename
-xg_clf.save_model(fname=model_path)
-
-# Log model to WandB run
-run.log_model(path=model_path, name=model_basename)
-
-# Log plots to WandB run
-roc_curve_and_auc(y_val, y_probas[:, 1], log_to_run=run)
-goal_rate_by_percentile(y_val, y_probas[:, 1], log_to_run=run)
-cumulative_proportion_of_goals(y_val, y_probas[:, 1], log_to_run=run)
-reliability_diagram(y_val, y_probas[:, 1], log_to_run=run)
-
-# Random Baseline generation and log to WandB
-y_probas_random = np.random.uniform(0, 1, len(y_val))
-
-roc_curve_and_auc(y_val, y_probas_random, log_to_run=run)
-goal_rate_by_percentile(y_val, y_probas_random, log_to_run=run)
-cumulative_proportion_of_goals(y_val, y_probas_random, log_to_run=run)
-
-# End run
-run.finish()
-
-'''
