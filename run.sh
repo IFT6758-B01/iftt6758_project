46d9ea815515ff355161756658a1dc996dd11354
linkai-aries-ma <arieslkm@gmail.com>
2024-12-14 21:11:09 -0500

    finished part 2-4 of milestone 3, including the flask app, the client and the docker deployment

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# echo "TODO: fill in the docker run command"
+
+docker run --name serving-client \
+    -e WANDB_API_KEY=$WANDB_API_KEY \
+    -p 5000:5000 \
+    flask-serving-client
