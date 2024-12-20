2d9f237528db1e044a2a2f92a428c869e8c05614
linkai-aries-ma <sheeple@Sheeplenator-Remastered>
2024-12-20 16:59:46 -0500

    added streamlit docker functionalities

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 
 # echo "TODO: fill in the docker build command"
 docker build -t flask-serving-client -f Dockerfile.serving .
+
+docker build -t streamlit-client -f Dockerfile.streamlit .
--- run.sh
+++ run.sh
@@ -6,3 +6,7 @@ docker run --name serving-client \
     -e WANDB_API_KEY=$WANDB_API_KEY \
     -p 5000:5000 \
     flask-serving-client
+
+docker run --name streamlit-client \
+    -p 8501:8501 \
+    streamlit-client
