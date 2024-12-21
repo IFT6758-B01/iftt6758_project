6fd5e0e4813e5b2cf1d2242d3e7ba5ae925ffd3a
liyuanmontreal <liyuan.montreal@gmail.com>
2024-12-21 18:10:38 -0500

    add error msg, add " with st.container"

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,39 +16,36 @@ os.sys.path.append((pathlib.Path(__file__) / '..' ).resolve().absolute())
 st.title("HOCKEY VIZ TOOL")
 
 URL = 'http://0.0.0.0:8080'
+URL_TEAMNAME = "https://api.nhle.com/stats/rest/en/team"
+URL_GAMESTORY = "https://api-web.nhle.com/v1/wsc/game-story/"
+
+with st.sidebar:
+    # Add input for the sidebar    
+    st.sidebar.header("Select model")
+    model_selection_form = st.sidebar.form('Model')
+    #model_selection_form.write('Default entity: IFT6758_2024-B01')
+    #workspace = model_selection_form.text_input('Workspace:', placeholder='ms2-logistic-regression')
+    workspace = model_selection_form.text_input('Workspace:', value='IFT6758_2024-B01')
+    model = model_selection_form.text_input('Model:', value='Distance_Angle')
+    model_version = model_selection_form.text_input('Version:', value='V0')
+    model_submit = model_selection_form.form_submit_button('Submit')
+    if model_submit and (model == '' or model_version == '' or workspace == ''):
+        model_selection_form.warning('Empty field Workspace/Model/Version')
+    if model_submit and (model != '' and model_version != '' and workspace != ''):
+        print(type(model))
+        print(type(model_version))
+        st.sidebar.write('Fetching model', model, 'with version', model_version, 'in', workspace)
+        model_selection_json = {
+            'workspace': workspace,
+            'model': model,
+            'version': model_version
+        }
+        response = requests.post(f"{URL}/download_registry_model", json = model_selection_json)
+        st.sidebar.write(response.status_code, response.reason)
+        if response.status_code != 200:      
+            st.sidebar.write(response.content)
 
 
-st.sidebar.header("Select model")
-model_selection_form = st.sidebar.form('Model')
-#model_selection_form.write('Default entity: IFT6758_2024-B01')
-#workspace = model_selection_form.text_input('Workspace:', placeholder='ms2-logistic-regression')
-workspace = model_selection_form.text_input('Workspace:', value='IFT6758_2024-B01')
-model = model_selection_form.text_input('Model:', value='Distance_Angle')
-model_version = model_selection_form.text_input('Version:', value='V0')
-model_submit = model_selection_form.form_submit_button('Submit')
-if model_submit and (model == '' or model_version == '' or workspace == ''):
-    model_selection_form.warning('Empty field Workspace/Model/Version')
-if model_submit and (model != '' and model_version != '' and workspace != ''):
-    print(type(model))
-    print(type(model_version))
-    st.sidebar.write('Fetching model', model, 'with version', model_version, 'in', workspace)
-    model_selection_json = {
-        'workspace': workspace,
-        'model': model,
-        'version': model_version
-    }
-    response = requests.post(f"{URL}/download_registry_model", json = model_selection_json)
-    st.sidebar.write(response.status_code, response.reason)
-    if response.status_code != 200:      
-        st.sidebar.write(response.content)
-#with st.sidebar:
-#    st.header("Select model")
-#    with st.form('Model'):
-#        model = st.text_input('Model:')
-#        version = st.text_input('Version:')
-#        submit = st.form_submit_button('Submit')
-#        if submit:
-#            st.write('Fetching game')
 
 #list_downloaded_games = pd.read_csv(pathlib.Path('./ift6758/dataset/complex_engineered/augmented_data.csv'))['game_id'].unique().tolist()
 @st.cache_data(max_entries=1)
@@ -58,133 +55,128 @@ def list_downloadable_games():
     return list_downloadable_games
 #list_available_games = list_downloaded_games + list_downloadable_games
 
-game_id_container = st.container()
-list_downloadable_games = list_downloadable_games()
-game_id_selectbox = game_id_container.selectbox('Game ID', list_downloadable_games)
-game_id_submit = game_id_container.button('Ping game')
-if game_id_submit:
-    request = { 'game_id': game_id_selectbox }
-    response = requests.post(URL+'/process_game', json=request)
-#with st.container():
-#    game_id = st.text('Game ID')
-#    list_available_games = pd.read_csv(pathlib.Path('./ift6758/dataset/complex_engineered/augmented_data.csv'))['game_id'].unique()
-#    game_id_entry = st.selectbox('Game ID', list_available_games)
-#    submit_gid = st.button('Ping game')
-#    if submit_gid:
-#        request = { 'game_id': game_id_entry }
-#        response = requests.post('http://0.0.0.0:8080/process_game', json=request)
-
-
-
-predictions_container = st.container()
-if not game_id_submit:
-    st.write('Waiting for game input')
-else:
-    response_json = response.json()
-    if response_json == '':
-        predictions_container.error('Got empty response, is game valid ?')
-    #st.write(response_json.get('message'))
-    if not str(response.status_code).startswith('2'):
-        with open('./serving/flask.log', 'r') as logs:
-            tail_logs = ''
-            for line in (logs.readlines() [-20:]):
-                tail_logs += line+'\n'
-            st.error('An error occured while generating predictions, please refer to logs\n\n')
-            st.error(tail_logs)
-    else:
-        if "xg_df" not in st.session_state:
-            df = pd.DataFrame(
-              data=[ (game_event.get('predicted_probabilities'), game_event.get('team_id')) for game_event in response_json ],
-              columns=['goal_proba', 'team_id']
-            )
-            df_all= pd.json_normalize(response_json)
-            
-            
-
-            home_game_xG = df.groupby(by='team_id').sum().reset_index()
-
-            # get all team names list
-            url = "https://api.nhle.com/stats/rest/en/team"
-            response = requests.get(url)
-            response.raise_for_status()
-            data = response.json()            
-            df_team = pd.json_normalize(data['data'])
-           
-
-            # add one column 'full Name'  
-            home_game_xG['name'] = home_game_xG['team_id'].map(df_team.set_index('id')['fullName'])
-            #home_game_xG 
-
-            # print team name
-            team_1 = home_game_xG.at[0, 'name']
-            team_2 = home_game_xG.at[1, 'name']
-            #st.write(f"Game: {game_id_selectbox }  {team_1} vs {team_2}")
-            #team_title = f"<p style='font-family:sans-serif; color:Black; font-size: 20px;'>Game: {game_id_selectbox }   {team_1} vs {team_2}</p>"
-            #st.markdown(team_title, unsafe_allow_html=True)
-            st.write(f"Game: {game_id_selectbox}")
-            st.header(f"{team_1} VS {team_2}")
-
-            #home_game_xG
-            st.session_state.xg_df = home_game_xG
-            #away_game_xG·=·
-
-            # Get period and left time from last row as current period          
-            last_period = df_all.iloc[-1]['period'] # -1 for last row
-            last_time_remaining = df_all.iloc[-1]['time_remaining']
-            st.write(f"Period: {last_period}, Time: {last_time_remaining} - left")
-           
-
-            #print Score ( predict Score)      
-            #get socre info 
-            url = f"https://api-web.nhle.com/v1/wsc/game-story/{game_id_selectbox}"             
-            response = requests.get(url)
-            response.raise_for_status()
-            data = response.json() 
-            score_1 = round(data['awayTeam']['score'])
-            score_2 = round(data['homeTeam']['score'])      
-            predict_score_1 = round(home_game_xG.at[0, 'goal_proba'],1)
-            predict_score_2 = round(home_game_xG.at[1, 'goal_proba'],1)
-            diff_1= round(predict_score_1-score_1,1)
-            diff_2= round(predict_score_2-score_2,1)
-            col1, col2= st.columns(2)
-            with col1:            
-                st.metric(label=f"{team_1} Current(Predict)", value=f"{score_1}({predict_score_1})", delta=f"{diff_1}")
-            with col2:      
-                st.metric(label=f"{team_2}", value=f"{score_2}({predict_score_2})", delta=f"{diff_2}")
-            
-            # show data used for predictions
-            st.header(f"Data used for predictions")
-            df_all
-
- 
-
-
-
-
-          
-            
-
-
-        else:
-            st.session_state.xg_df
-#with st.container():
-    # TODO: Add Game info and predictions
-    #response_json = json.dumps(response)
-
-    #df = pd.DataFrame(
-    #  data=[ (game_event.get('predicted_probabilities'), game_event.get('team_id')) for game_event in response_json ],
-    #  columns=['goal_proba', 'team_id']
-    # )
-    #home_game_xG = df.groupby(by='team_id').sum()
-    #away_game_xG = 
-#    st.write('home_team')
-#    st.write('away_team')
 
 with st.container():
-    # TODO: Add data used for predictions
-    with st.expander('Data'):
-        pass
-        #response.game_df
-    with st.expander('Input features'):
-        pass
-        #response.game_df[['distance_from_net', 'angle_from_net']]
+    # Add Game ID input  
+    game_id_container = st.container()
+    list_downloadable_games = list_downloadable_games()
+    game_id_selectbox = game_id_container.selectbox('Game ID', list_downloadable_games)
+    game_id_submit = game_id_container.button('Ping game')
+    if game_id_submit:
+        request = { 'game_id': game_id_selectbox }
+        response = requests.post(URL+'/process_game', json=request)
+
+
+
+with st.container():
+    #Add Game info,predictions,data   
+    predictions_container = st.container()
+    if not game_id_submit:
+        st.write('Waiting for game input')
+    else:
+        response_json = response.json()
+        if response_json == '':
+            predictions_container.error('Got empty response, is game valid ?')
+        #st.write(response_json.get('message'))
+        if not str(response.status_code).startswith('2'):
+            with open('./serving/flask.log', 'r') as logs:
+                tail_logs = ''
+                for line in (logs.readlines() [-20:]):
+                    tail_logs += line+'\n'
+                st.error('An error occured while generating predictions, please refer to logs\n\n')
+                st.error(tail_logs)
+        else:
+            if "xg_df" not in st.session_state:
+                df = pd.DataFrame(
+                data=[ (game_event.get('predicted_probabilities'), game_event.get('team_id')) for game_event in response_json ],
+                columns=['goal_proba', 'team_id']
+                )
+                df_all= pd.json_normalize(response_json) 
+
+                home_game_xG = df.groupby(by='team_id').sum().reset_index()               
+
+                # get all team names list  
+                try:
+                    response = requests.get(URL_TEAMNAME )
+                    response.raise_for_status()
+                    data = response.json()            
+                    df_team = pd.json_normalize(data['data'])
+                except requests.Timeout:
+                    print(f"Request to {URL_TEAMNAME } timed out.") 
+                except ConnectionError:
+                    print(f"Error connecting to {URL_TEAMNAME }. Check your internet connection.")  
+                except requests.HTTPError as http_err:
+                    print(f"HTTP error occurred: {http_err}")
+                    if response.status_code == 404:
+                        print(f"Resource not found at {URL_TEAMNAME }") #More specific error message
+                except requests.RequestException as req_err:  # Catch other request exceptions
+                    print(f"A request error occurred: {req_err}")                  
+                except ValueError as json_err: #Catch json decoding errors
+                    print(f"Decoding JSON failed: {json_err}")
+                    print(f"Response text: {response.text}") #Print response text to debug                 
+                except Exception as e: #Catch other errors
+                    print(f"An unexpected error occurred: {e}")
+
+                # add one column 'name'  
+                home_game_xG['name'] = home_game_xG['team_id'].map(df_team.set_index('id')['fullName'])
+                #home_game_xG 
+
+                # print team name
+                team_1 = home_game_xG.at[0, 'name']
+                team_2 = home_game_xG.at[1, 'name']                
+                st.write(f"Game: {game_id_selectbox}")
+                st.header(f"{team_1} VS {team_2}")
+
+                
+                st.session_state.xg_df = home_game_xG
+                #away_game_xG·=·
+
+                # Get period and left time from last row as current period          
+                last_period = df_all.iloc[-1]['period'] # -1 for last row
+                last_time_remaining = df_all.iloc[-1]['time_remaining']
+                st.write(f"Period: {last_period}, Time: {last_time_remaining} - left")            
+
+                #print Score ( predict Score)      
+                #get socre info 
+                url_gamestory = f"{URL_GAMESTORY}{game_id_selectbox}"     
+                try:     
+                    response = requests.get(url_gamestory)
+                    response.raise_for_status()
+                    data = response.json() 
+                    score_1 = round(data['awayTeam']['score'])
+                    score_2 = round(data['homeTeam']['score'])      
+                    predict_score_1 = round(home_game_xG.at[0, 'goal_proba'],1)
+                    predict_score_2 = round(home_game_xG.at[1, 'goal_proba'],1)
+                    diff_1= round(predict_score_1-score_1,1)
+                    diff_2= round(predict_score_2-score_2,1)
+                    col1, col2= st.columns(2)
+                    with col1:            
+                        st.metric(label=f"{team_1} Current(Predict)", value=f"{score_1}({predict_score_1})", delta=f"{diff_1}")
+                    with col2:      
+                        st.metric(label=f"{team_2}", value=f"{score_2}({predict_score_2})", delta=f"{diff_2}")
+                except requests.Timeout:
+                    print(f"Request to { url_gamestory } timed out.") 
+                except ConnectionError:
+                    print(f"Error connecting to { url_gamestory  }. Check your internet connection.")  
+                except requests.HTTPError as http_err:
+                    print(f"HTTP error occurred: {http_err}")
+                    if response.status_code == 404:
+                        print(f"Resource not found at { url_gamestory }") #More specific error message
+                except requests.RequestException as req_err:  # Catch other request exceptions
+                    print(f"A request error occurred: {req_err}")                  
+                except ValueError as json_err: #Catch json decoding errors
+                    print(f"Decoding JSON failed: {json_err}")
+                    print(f"Response text: {response.text}") #Print response text to debug                 
+                except Exception as e: #Catch other errors
+                    print(f"An unexpected error occurred: {e}")
+                
+                # show data used for predictions
+                with st.expander('Data'): 
+                    # show data used for predictions
+                    st.header(f"Data used for predictions")
+                    df_all
+            else:
+                st.session_state.xg_df
+
+
+      
